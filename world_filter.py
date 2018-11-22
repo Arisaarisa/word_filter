5978e0731785c62b495458c7fe6b07eca8876781
arisa <arisankochira10@gmail.com>
2018-11-22 22:28:44 +0900

    C1やっとできた!

--- world_filter.py
+++ world_filter.py
@@ -1,32 +1,15 @@
-# class WordFilter:
-#     def __int__(self, ng_word):
-#         self.ng_word = ng_word
-#
-#     def detect(self):
-#         return self.ng_word
-#
-#         print(self.detect())
-#
-#
-# if __name__ == "__main__":
-#     my_filter = WordFilter("assena-ru")
-#     my_filter.detect()
 class WordFilter:
     def __init__(self, ng_word):
         self.ng_word = ng_word
 
-    def detect(self):
-        if self.ng_word == "アーセナル":
-            print(True)
+    def detect(self, word):
+        if self.ng_word in word:
             return True
         else:
-            print(False)
             return False
 
-if __name__ == "__main__":
-    my_filter = WordFilter("アーセナル")
-    my_filter.detect()
 
-print("アーセナル")
+my_filter = WordFilter('アーセナル')
 
-print("リバプール")
+print(my_filter.detect('昨日のアーセナル戦はアツかった!'))
+print(my_filter.detect('昨日のリバプール戦はアツかった!'))
